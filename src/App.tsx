ccc52dcb2c52cf887935234db8c33097eb532251
EdenPark <edengoforit@gmail.com>
2023-01-16 21:10:49 +1300

    tailcss setup and styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import './App.css';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+import { HomePage } from './app/containers/HomePage';
+
+const AppContainer = styled.div`
+	${tw`
+		w-full
+		h-full
+		flex
+		flex-col
+	`}
+`;
 
 function App() {
 	return (
-		<div className='App'>
-			<h1>Hello World</h1>
-		</div>
+		<AppContainer>
+			<HomePage />
+		</AppContainer>
 	);
 }
 
