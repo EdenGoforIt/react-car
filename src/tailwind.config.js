e60765ef922527539a3eb4740407588300da4dcc
EdenPark <edengoforit@gmail.com>
2023-01-17 08:01:03 +1300

    added tailwind css config media

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			sm: '640px',
+			// => min width :640px
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+			'2xl': '1536px',
+		},
 	},
 	plugins: [],
 };
