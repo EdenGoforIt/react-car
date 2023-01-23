1031a920b1394f6d8104b0bd4bd988f2d3467bf3
EdenPark <edengoforit@gmail.com>
2023-01-24 07:43:42 +1300

    burger menu style tidy up

--- src/app/components/navBar/navItems.tsx
+++ src/app/components/navBar/navItems.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import tw from 'twin.macro';
+import { slide as Menu } from 'react-burger-menu';
+import { useMediaQuery } from 'react-responsive';
+import { SCREENS } from '../responsive';
+import menuStyle from './menuStyle';
+
 const ListContainer = styled.ul`
 	${tw`
         flex
@@ -8,7 +13,7 @@ const ListContainer = styled.ul`
     `}
 `;
 
-const NavItem = styled.li`
+const NavItem = styled.li<{ menu?: any }>`
 	${tw`
         text-xs
         md:text-base
@@ -22,9 +27,50 @@ const NavItem = styled.li`
         ease-in-out
         hover:text-gray-700
     `}
+	& > a {
+		color: white;
+		text-decoration: none;
+	}
+
+	${({ menu }) =>
+		menu &&
+		css`
+			${tw`
+			text-white
+			text-xl
+			mb-3
+			focus:text-white
+
+		`};
+		`}
 `;
 
 export function NavItems() {
+	const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
+	if (isMobile) {
+		return (
+			<Menu right styles={menuStyle}>
+				<ListContainer>
+					<NavItem menu>
+						<a href='#'>Home</a>
+					</NavItem>
+					<NavItem menu>
+						<a href='#'>Cars</a>
+					</NavItem>
+					<NavItem menu>
+						<a href='#'>Services</a>
+					</NavItem>
+					<NavItem menu>
+						<a href='#'>Career</a>
+					</NavItem>
+					<NavItem menu>
+						<a href='#'>Contact Us</a>
+					</NavItem>
+				</ListContainer>
+			</Menu>
+		);
+	}
+
 	return (
 		<>
 			<ListContainer>
---
+++ src/app/components/responsive/index.tsx
@@ -0,0 +1,8 @@
+export const SCREENS = {
+	sm: '640px',
+	// => min width :640px
+	md: '768px',
+	lg: '1024px',
+	xl: '1280px',
+	'2xl': '1536px',
+};
