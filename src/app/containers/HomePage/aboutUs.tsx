b37cf0831b15aa2b44d3fb1c5ee256cc1fe66f54
EdenPark <edengoforit@gmail.com>
2023-02-27 08:12:08 +1300

    About us container added

--- src/app/containers/HomePage/aboutUs.tsx
+++ src/app/containers/HomePage/aboutUs.tsx
@@ -1,9 +1,97 @@
 import React from "react";
 import styled from "styled-components";
+import tw from "twin.macro";
+import { SCREENS } from "../../components/responsive";
+import JeepImage from "../../assets/images/jeep.png";
 
-const AboutUsContainer = styled.div``;
+const AboutUsContainer = styled.div`
+  ${tw`
+        w-full
+        flex
+        flex-wrap
+        items-center
+        2xl:justify-center
+        pt-4
+        pr-7
+        pl-7
+        md:pl-0
+        md:pr-0
+        bg-white
+
+    `}
+`;
+
+const CarContainer = styled.div`
+  width: auto;
+  height: 15em;
+  margin-left: -30px;
+  img {
+    width: auto;
+    height: 100%;
+  }
+
+  @media (min-width: ${SCREENS.md}) {
+    height: 28em;
+  }
+  @media (min-width: ${SCREENS.lg}) {
+    height: 30em;
+  }
+  @media (min-width: ${SCREENS["2xl"]}) {
+    height: 35em;
+  }
+`;
+const InfoContainer = styled.div`
+  ${tw`
+    w-1/2
+    flex
+    flex-col
+    md:ml-6
+    2xl:ml-16
+  `}
+`;
+
+const Title = styled.h1`
+  ${tw`
+        text-black
+        text-2xl
+        md:text-5xl
+        font-extrabold
+        md:font-black
+        md:leading-normal
+    `}
+`;
+
+const InfoText = styled.p`
+  ${tw`
+        max-w-2xl
+        text-sm
+        md:text-base
+        text-gray-500
+        font-normal
+        mt-4
+    `}
+`;
 
 export function AboutUs() {
-
-    
+  return (
+    <AboutUsContainer>
+      <CarContainer>
+        <img src={JeepImage} />
+      </CarContainer>
+      <InfoContainer>
+        <Title>Feel the best Experience with our Rental Deals</Title>
+        <InfoText>
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using 'Content here, content here', making it
+          look like readable English. Many desktop publishing packages and web
+          page editors now use Lorem Ipsum as their default model text, and a
+          search for 'lorem ipsum' will uncover many web sites still in their
+          infancy. Various versions have evolved over the years, sometimes by
+          accident, sometimes on purpose (injected humour and the like).
+        </InfoText>
+      </InfoContainer>
+    </AboutUsContainer>
+  );
 }
--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -4,6 +4,7 @@ import tw from "twin.macro";
 import { BookCard } from "../../components/bookCard";
 import { Marginer } from "../../components/marginer";
 import { Navbar } from "../../components/navBar";
+import { AboutUs } from "./aboutUs";
 import { BookingSteps } from "./bookingSteps";
 import { TopSection } from "./topSection";
 
@@ -27,6 +28,8 @@ export function HomePage() {
       <BookCard />
       <Marginer direction="vertical" margin="5em" />
       <BookingSteps />
+      <Marginer direction="vertical" margin="5em" />
+      <AboutUs />
     </PageContainer>
   );
 }
